060b5451954de56952144c12f1d3fa89bd719461
Asayxru <arseniishevchuk1@gmail.com>
2026-01-22 19:22:23 +0200

    Base

---
+++ src/test/java/com/kpi/cosmocats/cosmocatsapi/CosmoCatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpi.cosmocats.cosmocatsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoCatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
