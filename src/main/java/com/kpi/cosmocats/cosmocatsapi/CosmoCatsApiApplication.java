5ee56abe9292550e2ea292cc6103db00a591e4f9
Asayxru <arseniishevchuk1@gmail.com>
2026-01-22 19:49:17 +0200

    migrated to gradle

--- src/main/java/com/kpi/cosmocats/cosmocatsapi/CosmoCatsApiApplication.java
+++ src/main/java/com/kpi/cosmocats/cosmocatsapi/CosmoCatsApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CosmoCatsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CosmoCatsApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CosmoCatsApiApplication.class, args);
+    }
 
 }
